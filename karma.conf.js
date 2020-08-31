d87def0925efe1ed1587bbb4ed1037c0c3a7e5b9
Edward <edexorsoft@gmail.com>
2020-08-30 21:00:14 -0600

    final

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/Desafio1'),
+      dir: require('path').join(__dirname, './coverage/SD172030'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
